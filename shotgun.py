2454bfe232dd6a464c53923e61623986f1031700
Nacho <0163473@up.edu.mx>
2017-06-11 01:12:29 -0500

    Code Name

--- shotgun.py
+++ shotgun.py
@@ -1,8 +1,10 @@
+import os
 import time
 from shotgun_api3 import Shotgun 
 
 inputType = None
 inputID = None
+inputCode = None
 requestVersions = None
 
 sg = Shotgun("https://upgdl.shotgunstudio.com",
@@ -14,9 +16,11 @@ def typeVerification(typeToVerify):
 	global inputType 
 	if typeToVerify == 'asset':
 		inputType = 'Asset'
+		os.system('CLS')
 		return True
 	elif typeToVerify == 'shot':
 		inputType = 'Shot'
+		os.system('CLS')
 		return True
 	else:
 		return False
@@ -31,6 +35,7 @@ def idVerification(idToVerify):
 	if request != None:
 		inputID = idToVerify
 		print "El Id %s pertenece al %s %s" %(inputID, inputType, request['code'])
+		os.system('CLS')
 		return True
 	else: 
 		return False
@@ -42,6 +47,26 @@ def findVersions():
 	print "Versiones creadas en el %s seleccionado:\n" %inputType
 	for version in requestVersions:
 		print version['code']
+def assignNewName():
+	global requestVersions, inputCode
+	newNameOption = raw_input("Desea crear un nuevo nombre? (s/n)\n")
+	if newNameOption == 'n':
+		inputCode = requestVersions[len(requestVersions)-1]['code']
+		inputCode = "%s%03d" %(inputCode[:len(inputCode)-3], int(inputCode[len(inputCode)-3:])+1)
+		print inputCode
+	else:
+		possibleCode = raw_input("Escriba el nombre a asignar\n")
+		inputCode = "%s v001" %possibleCode
+		for version in requestVersions:
+			if possibleCode.lower() in version["code"].lower():
+				inputCode = "%s%03d" %(version["code"][:len(version["code"])-3], int(version["code"][len(version["code"])-3:])+1)
+	confirmCode = raw_input("El nombre de la nueva version sera:\n%s\nEsta de acuerdo? (s/n)" %inputCode)
+	if confirmCode.lower() == 'n':
+		return False
+	else:
+		os.system('CLS')
+		return True
+
 
 """Analizar respuesta: Asset o Shot"""
 while not typeVerification(raw_input("Elige una de las opciones:\n-Asset\n-Shot\n").lower()):
@@ -50,5 +75,6 @@ while not typeVerification(raw_input("Elige una de las opciones:\n-Asset\n-Shot\
 while not idVerification(raw_input("Ahora introduzca el ID del %s:\n" %inputType)):
 	print "\n---Tipo de dato invalido o inexistente. Vuelva a intentarlo---\n"
 findVersions()
-
+while not assignNewName():
+	print ''
 time.sleep(5)
